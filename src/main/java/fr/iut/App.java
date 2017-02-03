2689ef7a4a678b1fc2d1c48dd2279656dde13939
ShellCode33 <clement.fleury33@gmail.com>
2017-02-03 16:52:43 +0100

    update pom xml

--- src/main/java/fr/iut/App.java
+++ src/main/java/fr/iut/App.java
@@ -1,13 +1,7 @@
 package fr.iut;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println( "Hello World!" );
     }
 }
