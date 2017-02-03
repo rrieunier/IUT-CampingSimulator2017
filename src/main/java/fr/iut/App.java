07f3d3871ea6b038efdd2ecebc819725f77bbba8
ShellCode33 <clement.fleury33@gmail.com>
2017-02-03 22:37:08 +0100

    pom xml configured + Begin code

--- src/main/java/fr/iut/App.java
+++ src/main/java/fr/iut/App.java
@@ -1,7 +1,39 @@
 package fr.iut;
 
-public class App {
+import fr.iut.view.ConnectionView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
+    private Stage stage;
+
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
+        primaryStage.setTitle("Camping Simulator 2017");
+        Scene scene = new Scene(new ConnectionView(this), SCREEN_WIDTH, SCREEN_HEIGHT, false);
+        primaryStage.setScene(scene);
+
+        primaryStage.setHeight(SCREEN_HEIGHT);
+        primaryStage.setWidth(SCREEN_WIDTH);
+        primaryStage.show();
+    }
+
+    @SuppressWarnings("unused")
+    public boolean tryLogin(String username, String password) {
+        //TODO : demander à la base de données si les identifiants sont bons, s'il ne le sont pas, la fonction retourne false et ConnectionView affiche un message d'erreur
+        return false;
+    }
+
+    public void GO_MAMENE_GOOOOOOOOOO() {
+        //TODO : Ici on fait du sale genre on affiche la bonne grosse fenetre de gestion du camping à l'ancienne tmtc
     }
 }
---
+++ src/main/java/fr/iut/view/ConnectionView.java
@@ -0,0 +1,33 @@
+package fr.iut.view;
+
+import fr.iut.App;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+/**
+ * Created by shellcode on 2/3/17.
+ */
+public class ConnectionView extends StackPane {
+
+    App app;
+
+    public ConnectionView(App app) { //TODO : T'as une instance du controller pour faire un app.tryLogin(user, pass) quand l'utilisateur clique sur le bouton pour se co
+        this.app = app;
+
+        //TODO : Maxime tu sais ce qu'il te reste à faire ;)
+
+        Button connectionButton = new Button();
+        connectionButton.setText("Connect");
+        connectionButton.setOnAction(event -> connectionButtonAction("input login", "input password"));
+        getChildren().add(connectionButton);
+    }
+
+    private void connectionButtonAction(String username, String password) {
+        boolean connected = app.tryLogin(username, password);
+
+        if(connected)
+            app.GO_MAMENE_GOOOOOOOOOO();
+        else
+            ;//TODO : Petit message des familles à l'ancienne ou petit popup c'est toi qui vois mamen, et t'as intérêt de bosser entre 2 pistes de ski. PS : j'espère que tu vas tomber grosse merde
+    }
+}
