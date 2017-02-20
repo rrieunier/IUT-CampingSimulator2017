3d60cc92f62df346bcaa3d037eab0dc2db500e42
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-02-20 20:46:09 +0100

    More test for DAO

--- src/test/java/fr/iut/persistence/dao/impl/DAOTest.java
+++ src/test/java/fr/iut/persistence/dao/impl/DAOTest.java
@@ -1,6 +1,8 @@
 package fr.iut.persistence.dao.impl;
 
+import fr.iut.persistence.entities.Authorization;
 import fr.iut.persistence.entities.Client;
+import fr.iut.persistence.entities.Employee;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
@@ -29,15 +31,52 @@ public class DAOTest extends TestCase{
 
     public void testPersist() throws Exception {
 
-        GenericDAOImpl<Client, Integer> dao = new GenericDAOImpl<>(Client.class);
+        GenericDAOImpl<Client, Integer> dao = new GenericDAOImpl<>(Client.class, "CampingUnitTests");
+        dao.open();
 
         Client client = new Client();
         client.setFirstname("azerztery");
         client.setLastname("azerztery");
 
-        dao.persist(client);
-        assertEquals(1, dao.findAll().size());
-        dao.delete(client);
-        assertEquals(0, dao.findAll().size());
+        assertTrue(dao.persist(client));
+        assertNotNull(dao.findById(client.getId()));
+        assertTrue(dao.delete(client));
+
+        dao.close();
+    }
+
+    public void testRelationsPerist() throws Exception {
+
+        GenericDAOImpl<Authorization, String> daoAuth =
+                new GenericDAOImpl<>(Authorization.class, "CampingUnitTests");
+
+        daoAuth.open();
+
+        Authorization authorization = new Authorization();
+        authorization.setLabel("auth");
+
+        assertTrue(daoAuth.persist(authorization));
+        assertNotNull(daoAuth.findById("auth"));
+
+        daoAuth.close();
+
+        GenericDAOImpl<Employee, Integer> daoEmp =
+                new GenericDAOImpl<>(Employee.class, "CampingUnitTests");
+        daoEmp.open();
+
+        Employee employee = new Employee();
+        employee.setLastName("ertrzezrtr");
+        employee.setFirstName("zaergdsfsgtrq");
+        employee.setLogin("aereterez");
+        employee.setPassword("zerzegfsf");
+        employee.getAuthorizations().add(authorization);
+
+        assertTrue(daoEmp.persist(employee));
+        assertNotNull(daoEmp.findById(employee.getId()));
+        assertTrue(daoEmp.update(employee));
+
+        assertTrue(daoEmp.findById(employee.getId()).getAuthorizations().contains(authorization));
+
+        daoEmp.close();
     }
 }
