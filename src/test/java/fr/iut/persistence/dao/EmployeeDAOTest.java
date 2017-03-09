a1834c7e178bf6459311e3945725004ee9928c95
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-09 11:44:51 +0100

    Added a dummy data main to insert dummy data in the databases for view tests

---
+++ src/test/java/fr/iut/persistence/dao/EmployeeDAOTest.java
@@ -0,0 +1,35 @@
+package fr.iut.persistence.dao;
+
+import org.junit.Test;
+
+/**
+ * Created by Sydpy on 3/8/17.
+ */
+public class EmployeeDAOTest {
+
+    @Test
+    public void connectUser() throws Exception {
+
+    }
+
+    @Test
+    public void findByLogin() throws Exception {
+
+    }
+
+    @Test
+    public void findByFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void findByLastName() throws Exception {
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        HibernateUtil.shutdown();
+        super.finalize();
+    }
+}
