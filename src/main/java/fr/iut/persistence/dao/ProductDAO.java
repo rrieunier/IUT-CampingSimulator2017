17ae9472cf5f812de44464faf6a67a9c727938e8
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-16 11:20:37 +0100

    ProductDAO update fixed

--- src/main/java/fr/iut/persistence/dao/ProductDAO.java
+++ src/main/java/fr/iut/persistence/dao/ProductDAO.java
@@ -2,8 +2,6 @@ package fr.iut.persistence.dao;
 
 import fr.iut.persistence.entities.Product;
 
-import javax.transaction.Transactional;
-
 /**
  * Created by Sydpy on 3/16/17.
  */
@@ -19,24 +17,32 @@ public class ProductDAO extends GenericDAO<Product, Integer> {
 
 
     @Override
-    @Transactional(rollbackOn = Exception.class)
     public Product update(Product entity) {
 
-        String query = "update Product set " +
-                "stock = :stock, " +
-                "criticalQuantity = :criticalQuantity, " +
-                "name = :name, " +
-                "sellPrice = :sellPrice " +
-                "where id = :id";
+        session.beginTransaction();
 
-        session.createQuery(query)
-                .setParameter("stock", entity.getStock())
-                .setParameter("criticalQuantity", entity.getCriticalQuantity())
-                .setParameter("name", entity.getName())
-                .setParameter("sellPrice", entity.getSellPrice())
-                .setParameter("id", entity.getId())
-                .executeUpdate();
+        try{
+            String query = "update Product set " +
+                    "stock = :stock, " +
+                    "criticalQuantity = :criticalQuantity, " +
+                    "name = :name, " +
+                    "sellPrice = :sellPrice " +
+                    "where id = :id";
 
+            session.createQuery(query)
+                    .setParameter("stock", entity.getStock())
+                    .setParameter("criticalQuantity", entity.getCriticalQuantity())
+                    .setParameter("name", entity.getName())
+                    .setParameter("sellPrice", entity.getSellPrice())
+                    .setParameter("id", entity.getId())
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
 
         return entity;
     }
