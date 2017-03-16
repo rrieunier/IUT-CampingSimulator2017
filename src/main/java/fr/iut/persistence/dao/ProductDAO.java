7b8d3449f5c850582dedb1d582387a55ae8be5ad
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-16 11:11:47 +0100

    ProductDAO created

---
+++ src/main/java/fr/iut/persistence/dao/ProductDAO.java
@@ -0,0 +1,40 @@
+package fr.iut.persistence.dao;
+
+import fr.iut.persistence.entities.Product;
+
+/**
+ * Created by Sydpy on 3/16/17.
+ */
+public class ProductDAO extends GenericDAO<Product, Integer> {
+
+
+    /**
+     * Constructor.
+     */
+    public ProductDAO() {
+        super(Product.class);
+    }
+
+
+    @Override
+    public Product update(Product entity) {
+
+        String query = "update Product set " +
+                "stock = :stock, " +
+                "criticalQuantity = :criticalQuantity, " +
+                "name = :name, " +
+                "sellPrice = :sellPrice " +
+                "where id = :id";
+
+        session.createQuery(query)
+                .setParameter("stock", entity.getStock())
+                .setParameter("criticalQuantity", entity.getCriticalQuantity())
+                .setParameter("name", entity.getName())
+                .setParameter("sellPrice", entity.getSellPrice())
+                .setParameter("id", entity.getId())
+                .executeUpdate();
+
+
+        return entity;
+    }
+}
