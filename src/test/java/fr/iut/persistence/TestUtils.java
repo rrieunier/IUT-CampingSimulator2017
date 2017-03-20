0731afa094d34b1d1b8dfd49b06d8f83543c609d
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-03-20 10:51:29 +0100

    renamed file

--- src/test/java/fr/iut/persistence/TestUtils.java
+++ src/test/java/fr/iut/persistence/TestUtils.java
@@ -1,9 +1,9 @@
-package fr.iut.persistence.dao;
+package fr.iut.persistence;
 
 /**
  * Created by Sydpy on 3/13/17.
  */
-public class DAOTestUtils {
+public class TestUtils {
 
     public static String randomString(){
         return Long.toHexString(Double.doubleToLongBits(Math.random()));
